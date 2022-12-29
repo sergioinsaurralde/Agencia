f1798b3ab8154f036652ad3735cfd2efdac51d3f
Sergio Insaurralde <srinsaurralde@gmail.com>
2022-12-29 19:24:33 -0300

    Config Home base

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,10 +3,26 @@ import {NavLink, Link} from 'react-router-dom';
 
 
 function Navbar(){
+
+    
+    window.onscroll = function() {scrollFunction()}
+
+    function scrollFunction() {
+        if(document.getElementById('navbar')){
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                document.getElementById('navbar').classList.add('shadow-navbar');
+                document.getElementById('navbar').classList.add('bg-white');
+            }else{
+                document.getElementById('navbar').classList.remove('shadow-navbar');
+                document.getElementById('navbar').classList.remove('bg-white');
+            }
+        }
+    }
+
     return(
-        <nav className='w-full py-4 shadow-md fixed'>
-            <div className=" bg-white px-4 sm:px-6">
-            <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap px-12">
+        <nav id='navbar' className='w-full py-6 top-0 transition duration-300 ease-in-out z-40 fixed'>
+            <div className=" px-4 sm:px-6">
+            <div className="-ml-4 -mt-2 hidden lg:flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
             <Link to='/' className="ml-4 mt-2">
             <img
                         src={'https://bafybeiczl4dcxupma2zeyilkukfl4yge64axnhajd722wxgin62mtts6uy.ipfs.w3s.link/murkivamarketing.png'}
@@ -22,13 +38,13 @@ function Navbar(){
                     <NavLink to='/carreras' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Carreras</NavLink>
                     <NavLink to='/blog' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Blog</NavLink>
                     <NavLink to='/contacto' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Contacto</NavLink>
-          <button
-            type="button"
-            className="inline-flex ml-12 items-center rounded-md border border-transparent bg-orange-button px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-gray-900 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-          >
-            Hire us
-        
-          </button>
+                    <Link
+                        to="/contacto"
+                        className="inline-flex ml-12 items-center rounded-md border border-transparent bg-orange-button px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-gray-900 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+                    >
+                        Hire Us
+                        
+                    </Link>
         </div>
       </div>
     </div>
