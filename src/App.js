6586f3565cf71dd0f74e8a2c8afc97413394a907
Sergio Insaurralde <srinsaurralde@gmail.com>
2022-12-29 23:01:11 -0300

    Config SEO

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import store from './store';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Provider } from "react-redux";
 import Error404 from "containers/erros/Error404";
 import Home from "containers/pages/Home";
@@ -12,7 +13,31 @@ import Contact from "containers/pages/Contact";
 
 function App() {
   return (
-    <Provider store={store}>
+    <HelmetProvider>
+      <Helmet>
+      <title>Murkiva | Software Agency</title>
+        <meta name="description" content="Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones." />
+        <meta name="keywords" content='agencia de software, agencia de marketing, creacion de pagina web' />
+        <meta name="robots" content='all' />
+        <link rel="canonical" href="https://www.murkiva.com/" />
+        <meta name="author" content='Murkiva' />
+        <meta name="publisher" content='Murkiva' />
+
+        {/* Social Media Tags */}
+        <meta property="og:title" content='Murkiva | Software Agency' />
+        <meta property="og:description" content='Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones.' />
+        <meta property="og:url" content="https://www.murkiva.com/" />
+        <meta property="og:image" content='https://bafybeicwrhxloesdlojn3bxyjqnxgsagtd4sl53a7t4cn4vfe2abmybzua.ipfs.w3s.link/lightbnuilbg.jpg' />
+
+        <meta name="twitter:title" content='Murkiva | Software Agency' />
+        <meta
+            name="twitter:description"
+            content='Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones.'
+        />
+        <meta name="twitter:image" content='https://bafybeicwrhxloesdlojn3bxyjqnxgsagtd4sl53a7t4cn4vfe2abmybzua.ipfs.w3s.link/lightbnuilbg.jpg' />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+      <Provider store={store}>
       <Router>
       <Routes>
         {/* Error Display */}
@@ -28,7 +53,9 @@ function App() {
         <Route path="/contacto" element={<Contact/>} />
       </Routes>
     </Router>
-    </Provider>
+      </Provider>
+    </HelmetProvider>
+    
     
   );
 }
